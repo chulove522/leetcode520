d8c463e372f70e8f91f7d73f369aade289afb1eb
yz <chulove522@hotmail.com.tw>
2022-03-12 18:04:37 +0800

    0312

--- Progarm.cs
+++ Progarm.cs
@@ -1,15 +1,59 @@
 ﻿using System;
 using System.Text;
+using firstspace;
 
 namespace happy0520
 {
     class Program
     {
+        static void Main02(string[] args)
+        {
+            Console.WriteLine("開始執行");
+            //leet520.leetcode520();
+            //BinaryTreeLevel.bstLevelOrder();
+            //BinaryTreeLevel.test(0);
+        }
+    }
+}
+class callmyclass
+{
+    public void calleveryone()
+    {
+        namespace01 ns01 = new namespace01();
+        Console.Write(this.ToString());
+        ns01.func1();
+    }
+}
+namespace firstspace
+{
+    class namespace01 { 
+        public void func1()
+        {
+            Console.WriteLine("inside first space");
+        }
+    }   
+}
+namespace secondspace
+{
+    class namespace01
+    {
+        public void func1()
+        {
+            Console.WriteLine("Inside second_space class可以同名喔");
+        }
+    }
+    public class testclass
+    {
         static void Main(string[] args)
         {
-            //leet520.leetcode520();
-            BinaryTreeLevel.bstLevelOrder();
-            //BinaryTreeLevel.test(0);
+            firstspace.namespace01 fc = new firstspace.namespace01();
+            secondspace.namespace01 sc = new secondspace.namespace01();
+
+            fc.func1();
+            sc.func1();
+            Console.ReadKey();
+            callmyclass cc = new callmyclass();
+            cc.calleveryone();
         }
     }
 }
---
+++ leet524.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace happy0520
+{
+    class leet524
+    {
+        //943
+        public string ShortestSuperstring(string[] words)
+        {
+            int n = words.Length;
+
+            //overlap
+            int[,] overlaps = new int[n,n];
+            for (int i = 0; i < n;  ++i)
+                for (int j = 0; j < n;  ++j) if( i != j)
+                {
+                        int m = Math.Min(words[i].Length, words[j].Length);
+                        for (int k = 0; k < m; k++)
+                        {
+                            if (words[i].EndsWith(words[j].Substring(0, k))){
+                                overlaps[i, j] = k;   //lcs
+                                break;
+                            }
+                        }
+                }
+                //dp[mask , i] =most overlap and end with word[i]
+                int[,] dp = new int[1<< n, n];   //n/2
+                int[,] parent = new int[1 << n, n];
+                for (int mask = 0; mask < (1 << n); mask++) {
+                    for (int e = 0; e < n; e++)
+                        parent[mask, e] = -1;
+
+                    for (int bit = 0; bit < n; ++bit) if (((mask >> bit) & 1) > 0)  //mask右移bit >-1(邊緣牆壁) 又!=0(有值)
+                    {                      
+                        int pmask = (int)Math.Pow(mask, bit);
+                        if (pmask == 0) continue;
+                            for (int i = 0; i < n; ++i) if(((pmask >> i) & 1) >0)  //如果pmask往右位移 >-1 (牆壁)又!=0(有值)
+                            {
+                                //針對pmask每一個檢查如果以word[i]作為結尾
+                                //那就補上後面剩餘的
+                                int val = dp[pmask, i] + overlaps[i, bit];
+                                if (val > dp[mask, bit]) {
+                                    dp[mask, bit] = val;
+                                    parent[mask, bit] = i;  //做一個紀錄
+                                }
+                            }
+                    }
+                }
+            //接下來要把解答建構出來
+            //word i - max(dp[-1]) //這句是?
+
+            
+            string ans="";
+            return ans;
+        }
+
+    }
+}
